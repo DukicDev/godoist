6bdeb848992af62401f2da115e9acfc35082f4b1
DukicDev <dukic.dev@gmail.com>
2024-12-26 17:05:42 +0100

    Add todoist api handler

---
+++ todoist/api.go
@@ -0,0 +1,54 @@
+package todoist
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+const baseUrl = "https://api.todoist.com/rest/v2/"
+
+type Client struct {
+	APIToken string
+}
+
+func NewClient(apiToken string) *Client {
+	return &Client{
+		APIToken: apiToken,
+	}
+}
+
+func (client *Client) makeRequest(method, endpoint string, body interface{}) ([]byte, error) {
+	url := baseUrl + endpoint
+	var reqBody []byte
+	var err error
+
+	if method != "GET" && body != nil {
+		reqBody, err = json.Marshal(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(reqBody))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", "Bearer "+client.APIToken)
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 400 {
+		return nil, fmt.Errorf("API returned error: %s", resp.Status)
+	}
+
+	return io.ReadAll(resp.Body)
+}
