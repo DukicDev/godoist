856acb022408bf45bdb4892a8bdc1ca4be3c138e
DukicDev <dukic.dev@gmail.com>
2024-12-26 17:37:08 +0100

    Add possibility for showing projects

--- cmd/list.go
+++ cmd/list.go
@@ -13,6 +13,7 @@ import (
 )
 
 var taskMaxLength int
+var showProjects bool
 
 // listCmd represents the list command
 var listCmd = &cobra.Command{
@@ -34,20 +35,29 @@ func init() {
 	// is called directly, e.g.:
 	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	listCmd.Flags().IntVarP(&taskMaxLength, "length", "l", 50, "set max length for task string")
+	listCmd.Flags().BoolVar(&showProjects, "show-projects", false, "set show-prjects to list tasks with projects (might take longer)")
 }
 
 func listRun(cmd *cobra.Command, args []string) {
-	tasks, err := client.GetTasks()
+	tasks, err := client.GetTasks(showProjects)
 	if err != nil {
 		log.Fatalf("error while listing Todos: %v\n", err)
 	}
 
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', 0)
 
-	fmt.Fprintln(w, "ID\tTask\tDescription\tDue Date")
+	if showProjects {
+		fmt.Fprintln(w, "ID\tTask\tDescription\tDue Date\tProject")
 
-	for i, task := range tasks {
-		fmt.Fprintf(w, "%d\t%s\t%s\t%s\n", i+1, task.ShortContent(taskMaxLength), task.Description, task.DueDate)
+		for i, task := range tasks {
+			fmt.Fprintf(w, "%d\t%s\t%s\t%s\t%s\n", i+1, task.ShortContent(taskMaxLength), task.Description, task.DueDate, task.Project)
+		}
+	} else {
+		fmt.Fprintln(w, "ID\tTask\tDescription\tDue Date")
+
+		for i, task := range tasks {
+			fmt.Fprintf(w, "%d\t%s\t%s\t%s\n", i+1, task.ShortContent(taskMaxLength), task.Description, task.DueDate)
+		}
 	}
 
 	w.Flush()
--- todoist/task.go
+++ todoist/task.go
@@ -3,16 +3,20 @@ package todoist
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 )
 
 type Task struct {
 	ID          string `json:"id"`
 	Content     string `json:"content"`
+	Priority    int    `json:"priority,omitempty"`
 	Description string `json:"description,omitempty"`
 	DueDate     string `json:"due_date,omitempty"`
+	ProjectID   string `json:"project_id,omitempty"`
+	Project     string `json:"-"`
 }
 
-func (c *Client) GetTasks() ([]Task, error) {
+func (c *Client) GetTasks(showProjects bool) ([]Task, error) {
 	data, err := c.makeRequest("GET", "tasks", nil)
 	if err != nil {
 		return nil, err
@@ -23,6 +27,12 @@ func (c *Client) GetTasks() ([]Task, error) {
 		return nil, fmt.Errorf("failed to decode tasks json: %v", err)
 	}
 
+	if showProjects {
+		for i := range tasks {
+			tasks[i].setProject(c)
+		}
+
+	}
 	return tasks, nil
 }
 
@@ -31,4 +41,18 @@ func (t *Task) ShortContent(maxLength int) string {
 		return t.Content[:maxLength-3] + "..."
 	}
 	return t.Content
+}
+
+func (t *Task) setProject(c *Client) {
+	if t.ProjectID != "" {
+		data, err := c.makeRequest("GET", "projects/"+t.ProjectID, nil)
+		if err != nil {
+			log.Printf("error while setting project for task: %v\n error: %v", t, err)
+		}
+		var project map[string]interface{}
+		if err := json.Unmarshal(data, &project); err != nil {
+			log.Printf("error while setting project for task: %v\n error: %v", t, err)
+		}
+		t.Project = project["name"].(string)
+	}
 }
